95006ef41c1cf6b8608d2b6ce352705e4df41445
Amaury WYART <amaury.wyart@gadz.org>
2017-06-19 19:19:01 +0200

    ajout day05 start

---
+++ day05/ex02/ex02.sql
@@ -0,0 +1,5 @@
+INSERT INTO ft_table (login, groupe, date) VALUES ("loki", "staff", "2013-05-01");
+INSERT INTO ft_table (login, groupe, date) VALUES ("scadoux", "student", "2014-01-01");
+INSERT INTO ft_table (login, groupe, date) VALUES ("chap", "staff", "2011-04-27");
+INSERT INTO ft_table (login, groupe, date) VALUES ("bambou", "staff", "2014-03-01");
+INSERT INTO ft_table (login, groupe, date) VALUES ("fantomet", "staff", "2010-04-03");
