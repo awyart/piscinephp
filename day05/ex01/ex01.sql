eddb770fc245a2886b47a067deb7d1232f11462e
awyart <awyart@student.42.fr>
2017-06-20 16:30:34 +0200

    day5

--- day05/ex01/ex01.sql
+++ day05/ex01/ex01.sql
@@ -1,2 +1,7 @@
-USE db_awyart;
-CREATE TABLE ft_table(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, login VARCHAR(255) NOT NULL, groupe VARCHAR(255) NOT NULL , date DATE NOT NULL);
+CREATE TABLE ft_table
+(
+  id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+  login VARCHAR(8) NOT NULL DEFAULT 'toto',
+  groupe ENUM('staff', 'student', 'other') NOT NULL,
+  date_de_creation DATE NOT NULL
+)
